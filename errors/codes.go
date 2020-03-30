f8d4e50c118482e5c9ff9a96c23a97eb24d8be0d
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-03-30 15:45:19 +0900

    Remove error code and message for empty field

--- errors/codes.go
+++ errors/codes.go
@@ -1,18 +1,16 @@
 package errors
 
 const (
-	EmailEmptyCode       = 100
+	UndefinedError = 0
+
 	EmailExistsCode      = 101
 	EmailFormatErrorCode = 102
 
-	UsernameEmptyCode       = 200
 	UsernameExistsCode      = 201
 	UsernameFormatErrorCode = 202
 
-	PasswordEmptyCode           = 300
 	PasswordTooShortCode        = 301
 	PasswordVulnerableErrorCode = 302
 
-	PublicKeyEmptyCode = 400
 	PublicKeyErrorCode = 402
 )
--- errors/messages.go
+++ errors/messages.go
@@ -2,18 +2,16 @@ package errors
 
 // Messages contains the error message for each error code
 var Messages = map[int]string{
-	EmailEmptyCode:       "Email field is empty.",
+	UndefinedError: "Undefined error",
+
 	EmailExistsCode:      "Email exists.",
 	EmailFormatErrorCode: "Email format error.",
 
-	UsernameEmptyCode:       "Username field is empty.",
 	UsernameExistsCode:      "Username exists.",
 	UsernameFormatErrorCode: "Username format error.",
 
-	PasswordEmptyCode:           "Password field is empty.",
 	PasswordTooShortCode:        "Password is too short.",
 	PasswordVulnerableErrorCode: "Password is vulnerable.",
 
-	PublicKeyEmptyCode: "PublicKey field is empty.",
 	PublicKeyErrorCode: "Not a proper public key.",
 }
