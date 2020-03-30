06133fa68adc8cfce2bbc912b0ab177df99e6bda
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-03-30 22:51:40 +0900

    Add a form Profile

---
+++ forms/profile.go
@@ -0,0 +1,11 @@
+package forms
+
+// Profile is a form for Profile
+type Profile struct {
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Nickname   string `json:"nickname"`
+	PictureURL string `json:"picture_url"`
+	Bio        string `json:"bio"`
+	MyKeys     string `json:"my_keys"`
+}
