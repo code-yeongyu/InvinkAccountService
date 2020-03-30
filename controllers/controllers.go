5c996af8642759d954be39968dc4b4ec7bb81302
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-03-30 22:08:09 +0900

    Update README.md

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,5 +1,13 @@
 package controllers
 
+import (
+	"invink/account-service/errors"
+	"net/http"
+	"unicode"
+
+	"github.com/gin-gonic/gin"
+)
+
 // Controller is a wrapper for all the controllers
 type Controller struct {
 }
@@ -7,4 +15,25 @@ type Controller struct {
 // NewController returns a new Controller instance
 func NewController() *Controller {
 	return &Controller{}
+}
+
+func abortWith400ErrorResponse(c *gin.Context, errorCode int) {
+	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": errorCode, "msg": errors.Messages[errorCode]})
+}
+
+func verifyPassword(s string) bool {
+	var number, lower, upper, special bool
+	for _, c := range s {
+		switch {
+		case unicode.IsNumber(c):
+			number = true
+		case unicode.IsLower(c):
+			lower = true
+		case unicode.IsUpper(c):
+			upper = true
+		case unicode.IsPunct(c) || unicode.IsSymbol(c):
+			special = true
+		}
+	}
+	return number && lower && upper && special
 }
--- controllers/register.go
+++ controllers/register.go
@@ -7,33 +7,11 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
-	"unicode"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func abortWith400ErrorResponse(c *gin.Context, errorCode int) {
-	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": errorCode, "msg": errors.Messages[errorCode]})
-}
-
-func verifyPassword(s string) bool {
-	var number, lower, upper, special bool
-	for _, c := range s {
-		switch {
-		case unicode.IsNumber(c):
-			number = true
-		case unicode.IsLower(c):
-			lower = true
-		case unicode.IsUpper(c):
-			upper = true
-		case unicode.IsPunct(c) || unicode.IsSymbol(c):
-			special = true
-		}
-	}
-	return number && lower && upper && special
-}
 
 // RegisterUser godoc
 // @Summary Register an user
