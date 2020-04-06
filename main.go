8b42c626853a66d9a6005434eadf7aa62207ee1a
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-04-06 23:16:56 +0900

    Update routes to use middlewares

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"invink/account-service/docs"
-	"invink/account-service/models"
 
 	_ "invink/account-service/docs"
 
@@ -13,7 +12,7 @@ func main() {
 	docs.SwaggerInfo.Title = "Invink Account / Profile Service API Documentation"
 	docs.SwaggerInfo.Description = "The Account/Profile Service API"
 	docs.SwaggerInfo.Version = "0.1"
-	docs.SwaggerInfo.Host = "not done yet, possibly invink.org"
+	docs.SwaggerInfo.Host = "possibly invink.org"
 	docs.SwaggerInfo.BasePath = "/v1"
 	docs.SwaggerInfo.Schemes = []string{"https"}
 	setupServer().Run()
@@ -21,11 +20,6 @@ func main() {
 
 func setupServer() *gin.Engine {
 	r := gin.Default()
-	db := models.Setup()
-	r.Use(func(c *gin.Context) {
-		c.Set("db", db)
-		c.Next()
-	})
 	setupRoutes(r)
 	return r
 }
--- routes.go
+++ routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"invink/account-service/controllers"
+	"invink/account-service/middlewares"
 
 	"github.com/gin-gonic/gin"
 
@@ -12,8 +13,21 @@ import (
 )
 
 func setupRoutes(r *gin.Engine) {
+	r.Use(middlewares.SetupDB)
 	c := controllers.NewController()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.POST("/register", c.RegisterUser)
 	r.POST("/auth", c.AuthUser)
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	profile := r.Group("/profile")
+	profile.Use(middlewares.AuthenticateJWT)
+	{
+		profile.GET("/", c.GetMyUsername)
+		profile.GET("/:username/", c.GetProfileByUsername)
+		profile.PATCH("/:username", nil)
+		profile.DELETE("/:username", nil)
+		profile.DELETE("/:username/:field_name", nil)
+	}
+
 }
