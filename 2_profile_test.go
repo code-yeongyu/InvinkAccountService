1b2eb485a9de57bbef77b90469c0d2a3ea7b2f50
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-04-12 15:01:05 +0900

    Add a test code for testing increase report attempt count feature

--- 2_profile_test.go
+++ 2_profile_test.go
@@ -422,6 +422,22 @@ func TestIncreaseCaptureAttemptRequest(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 1, int(response["capture_cnt"].(float64)))
 }
+func TestIncreaseReportAttemptRequest(t *testing.T) {
+	var response map[string]interface{}
+	w := performRequestWithHeader(ROUTER, "POST", "/attempt/report/", AUTHHEADER[0], nil)
+	assert.Equal(t, http.StatusOK, w.Code)
+	w = performRequestWithHeader(
+		ROUTER,
+		"GET",
+		"/profile/changer/",
+		AUTHHEADER[1],
+		nil,
+	)
+	assert.Equal(t, http.StatusOK, w.Code)
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, int(response["report_cnt"].(float64)))
+}
 func TestDeleteNicknameRequest(t *testing.T) {
 	w := performRequestWithHeader(
 		ROUTER,
