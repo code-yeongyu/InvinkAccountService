5448e9876bdc8199b4793d57de42668c21e72ed8
YeonGyu Kim <public.kim.yeon.gyu@gmail.com>
2020-04-12 14:41:57 +0900

    Add a test for capture attempt increase feature

--- 2_profile_test.go
+++ 2_profile_test.go
@@ -406,6 +406,22 @@ func TestVulerablePasswordProfilePatchRequest(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, errors.PasswordVulnerableCode, int(response["error"].(float64)))
 }
+func TestIncreaseCaptureAttemptRequest(t *testing.T) {
+	var response map[string]interface{}
+	w := performRequestWithHeader(ROUTER, "POST", "/capture/", AUTHHEADER[0], nil)
+	assert.Equal(t, http.StatusOK, w.Code)
+	w = performRequestWithHeader(
+		ROUTER,
+		"GET",
+		"/profile/changer/",
+		AUTHHEADER[1],
+		nil,
+	)
+	assert.Equal(t, http.StatusOK, w.Code)
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, int(response["capture_cnt"].(float64)))
+}
 func TestDeleteNicknameRequest(t *testing.T) {
 	w := performRequestWithHeader(
 		ROUTER,
